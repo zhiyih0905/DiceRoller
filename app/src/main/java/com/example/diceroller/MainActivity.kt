f1cebfdd29d93e4499064fd20b3f8f62bfa16e2f
taruc <fongzy-am17@student.tarc.edu.my>
2019-10-22 12:59:50 +0800

    DiceRoller

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import java.util.*
@@ -25,8 +26,20 @@ class MainActivity : AppCompatActivity() {
         val randomInt1 = Random().nextInt(6) + 1
         val randomInt2 = Random().nextInt(6) + 1
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
+        //val resultText: TextView = findViewById(R.id.result_text)
+        //resultText.text = randomInt.toString()
 
         val resultText1: TextView = findViewById(R.id.result_text1)
         resultText1.text = randomInt1.toString()
