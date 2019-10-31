cfc7bed65bc4455fa3535d132661bd185cfdba8b
Zhi Yih <zhiyih0905@gmail.com>
2019-11-01 00:57:29 +0800

    DiceRoller

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -36,15 +36,37 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
+        val diceImage1: ImageView = findViewById(R.id.dice_image1)
+        val drawableResource1 = when (randomInt1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val diceImage2: ImageView = findViewById(R.id.dice_image2)
+        val drawableResource2 = when (randomInt2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
         diceImage.setImageResource(drawableResource)
+        diceImage1.setImageResource(drawableResource1)
+        diceImage2.setImageResource(drawableResource2)
 
         //val resultText: TextView = findViewById(R.id.result_text)
         //resultText.text = randomInt.toString()
 
-        val resultText1: TextView = findViewById(R.id.result_text1)
-        resultText1.text = randomInt1.toString()
+        //val resultText1: TextView = findViewById(R.id.result_text1)
+        //resultText1.text = randomInt1.toString()
 
-        val resultText2: TextView = findViewById(R.id.result_text2)
-        resultText2.text = randomInt2.toString()
+        //val resultText2: TextView = findViewById(R.id.result_text2)
+        //resultText2.text = randomInt2.toString()
     }
 }
